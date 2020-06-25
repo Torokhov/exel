40f4a9a2c50979f3026721e52b672b38c4d44031
Oleg <torokhov093@gmail.com>
2020-06-25 14:32:40 +0700

    fix eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -74,12 +74,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: jsLoaders(),
-        loader: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
       },
       {
         test: /\.s[ac]ss$/i,
