9ad7edbed10b14b57b8407475aace732f570450c
Oleg <torokhov093@gmail.com>
2020-06-26 17:00:01 +0700

    add base framework

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './scss/index.scss';
+import {Exel} from '@/components/exel/Exel';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
 
-console.log('Working');
+const exel = new Exel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+
+exel.render();
